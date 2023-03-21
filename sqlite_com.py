b1ccf4aeb3788e2e371c2626fc6229d38d0b737d
LuisFPR04 <luisfpera@gmail.com>
2023-03-21 14:13:47 -0300

    commit

--- sqlite_com.py
+++ sqlite_com.py
@@ -27,4 +27,4 @@ def ReadBooks():
     res = cur.execute("SELECT * FROM books")
     #print(res.fetchall())
     for row in res:
-        print(row[0])
+        print(row[0], row[1], row[2], row[3], row[4], row[5])
