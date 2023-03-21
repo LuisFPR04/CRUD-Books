316e4d857d6286a960b6d6022db768d7bb8314ff
LuisFPR04 <luisfpera@gmail.com>
2023-03-21 13:20:07 -0300

    commit

--- sqlite_com.py
+++ sqlite_com.py
@@ -2,13 +2,27 @@ import sqlite3
 
 con = sqlite3.connect("booktable.db")
 cur = con.cursor()
-
 try:
-    #cur.execute("CREATE TABLE books (Name varchar(30) UNIQUE, Author varchar(30), Publisher varchar(10), Price int(3), ReleaseDate year, PageNumber int(4))")
-    cur.execute("INSERT INTO books (Name, Author, Publisher, Price, ReleaseDate, PageNumber) VALUES ('Shadows of Self', 'Brandon Sanderson', 'Nova', 30, '2015', 437)")
-    con.commit()
-except: print("Value already exists")
+    cur.execute("CREATE TABLE books (Name varchar(30) UNIQUE, Author varchar(30), Publisher varchar(10), Price int(3), ReleaseDate year, PageNumber int(4))")
+except: pass
 
+def AddBooks(Name, Author, Publisher, Price, RealeseDate, PageNumber):
+    try:
+        cur.execute(f"INSERT INTO books (Name, Author, Publisher, Price, ReleaseDate, PageNumber) VALUES ('{Name}', '{Author}', '{Publisher}', {Price}, '{RealeseDate}', {PageNumber})")
+        con.commit()
+    except: print("Value already exists")
+
+def DelBooks(value1, value2):
+    if type(value2) == str:
+        cur.execute(f"DELETE FROM BOOKS WHERE {value1} = '{value2}'")
+    if type(value2) == int:
+        cur.execute(f"DELETE FROM BOOKS WHERE {value1} > {value2}")
+    con.commit()
+    
+#def UpdateBooks(value1, value2, value3):
+#    cur.execute(f"UPDATE Books SET {value1} = '{value2}' WHERE = {value3}")
+
+AddBooks("Alloy of Law", "Brandon Sanderson", "Nova", 30, "2011", 403)
 res = cur.execute("SELECT * FROM books")
 
 print(res.fetchall())
