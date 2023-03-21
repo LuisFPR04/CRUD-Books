f5db72c7df92fc990ceb439774cbf34cf79e9b07
LuisFPR04 <luisfpera@gmail.com>
2023-03-21 12:36:42 -0300

    commit

--- sqlite_com.py
+++ sqlite_com.py
@@ -1,5 +1,14 @@
 import sqlite3
 
 con = sqlite3.connect("booktable.db")
+cur = con.cursor()
 
-print("Pepe")
+try:
+    #cur.execute("CREATE TABLE books (Name varchar(30) UNIQUE, Author varchar(30), Publisher varchar(10), Price int(3), ReleaseDate year, PageNumber int(4))")
+    cur.execute("INSERT INTO books (Name, Author, Publisher, Price, ReleaseDate, PageNumber) VALUES ('Shadows of Self', 'Brandon Sanderson', 'Nova', 30, '2015', 437)")
+    con.commit()
+except: print("Value already exists")
+
+res = cur.execute("SELECT * FROM books")
+
+print(res.fetchall())
