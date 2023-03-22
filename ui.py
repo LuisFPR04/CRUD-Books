f328f2f369fd8e3f4c50a55cd2d73a29d1312bf6
LuisFPR04 <luisfpera@gmail.com>
2023-03-22 11:28:56 -0300

    commit

--- ui.py
+++ ui.py
@@ -1,4 +1,12 @@
-import tkinter
+from tkinter import *
 from sqlite_com import ReadBooks, AddBooks
 
-ReadBooks()
+root = Tk()
+
+def MyClick():
+    myLabel = Label(root, text=ReadBooks)
+    myLabel.pack
+
+myButton = Button(root, text="Click", command=ReadBooks)
+myButton.pack()
+root.mainloop()
