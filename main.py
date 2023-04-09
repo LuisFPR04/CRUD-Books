fe1c147b8fba795ea186e317a50dc07a0ad23a9d
LuisFPR04 <luisfpera@gmail.com>
2023-04-09 14:51:12 -0300

    commit

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from CRUDbooktable import crud
 
 root = Tk()
 root.title("Librería Pepe")
+root.geometry("500x400")
 
-crudbutton = Button(root, text="Book table", command=crud).grid(column=0,row=0)
+title = Label(root, text="Librería Pepe").grid(column=1, row=0)
+crudbutton = Button(root, text="Book table", command=crud).grid(column=0,row=1)
 
 root.mainloop()
